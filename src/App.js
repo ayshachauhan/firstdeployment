5bdcfa39cdfd50ec4a08d9618418feabf350fd58
ayshachauhan <ayshachauhan0100@gmail.com>
2026-03-11 08:26:50 +0000

    change code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>React Multi-Environment App</h1>
+      <h1>React Multi-Environment App testtttttttttttttttttttttt</h1>
       <h2>Environment:</h2>
       <p style={{ color: "blue", fontWeight: "bold" }}>
         {environment || "No Environment Provided"}

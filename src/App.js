a5471eb2a0c59e0c4566f420c07c15970163c002
ayshachauhan <ayshachauhan0100@gmail.com>
2026-03-11 13:09:52 +0000

    code chnagebsajxhbsajxbhas

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>React Multi-Environment App testtttttttttttttttttttttt fghyujyhgtfdfgthyjuyhgtf</h1>
+      <h1>React Multi-Environment App 1552715271512715 testtttttttttttttttttttttt fghyujyhgtfdfgthyjuyhgtf</h1>
       <h2>Environment:</h2>
       <p style={{ color: "blue", fontWeight: "bold" }}>
         {environment || "No Environment Provided"}
